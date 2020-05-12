568b9100cdacbf0b2fd61798feccdcd85ebceb9f
jaek <jakethekoenig@gmail.com>
2020-05-12 13:41:44 -0700

    Clean up
    Cleaned up code by renaming and making spaces consistent, checking in
    statistics files, also deleted comparing two images of mouse three
    chamber and deleted mouse color threshold.

--- threechamber.py
+++ threechamber.py
@@ -3,71 +3,21 @@ import cv2
 from pickle import dump, load
 import os.path
 
-def drawtri():
-  img = Image.new('RGB', (100,100), (255,255,255))
-  draw = ImageDraw.Draw(img)
-  draw.polygon([(10,10),(10,90), (90,10)], fill=(200,20,20))
-  img.save("tri.jpg", "JPEG")
+mean_file = "statistics/mean.jpg"
+sd_file = "statistics/sd.jpg"
+z_dir = "z_pics/"
+z_pickle = 'zscore.pickle'
+points_file = 'statistics/mouse_location'
+chambers_file = 'statistics/mouse_chamber'
 
-def circlemouse():
-  im = Image.open("mouse.png")
-  img = threshold(im, (50,50,50))
-  img.save("mouse3.jpg", "JPEG")
-
-def threshold(im, thres):
-  img = Image.new('RGB',im.size, (255,255,255))
-  px = im.load()
-  px2 = img.load()
-  for i in range(im.size[0]):
-    for j in range(im.size[1]):
-      cur_color = px[i,j]
-      if cur_color[0]<thres[0] or cur_color[1]<thres[1] or cur_color[2]<thres[2]:
-        px2[i,j] = (0,0,0)
-      else:
-        px2[i,j] = (255,255,255)
-  return img
-
-def difference(im1,im2):
-  im = Image.new('RGB',im1.size, (255,255,255))
-  px1 = im1.load()
-  px2 = im2.load()
-  px3 = im.load()
-  for i in range(im.size[0]):
-    for j in range(im.size[1]):
-      color1 = px1[i,j]
-      color2 = px2[i,j]
-      if color1!=color2:
-        px3[i,j] = (0,0,0)
-      else:
-        px3[i,j] = (255,255,255)
-  return im
-
-def load_video(video):
-  vidcap = cv2.VideoCapture(video)
-  success,frame=vidcap.read()
-  last=frame
-  i = 0
-  while success:
-    i+=1
-    if i%50 == 3:
-      pil_image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-      pil_image=threshold(pil_image,(100,100,100))
-      pil_image2=Image.fromarray(cv2.cvtColor(last, cv2.COLOR_BGR2RGB))
-      pil_image2=threshold(pil_image2,(100,100,100))
-      output=difference(pil_image,pil_image2)
-      output.save("frames/diff"+str(i)+".jpg", "JPEG")
-    last=frame
-    success,frame = vidcap.read()
-
-  # Homework for Rhi!
-  # Make an image which is a red circle on a black background (Hint: consider drawtri)
-  # Color invert the mouse threshold
-  # Make some generative art!
-  # Things to learn about: for loops, if statements, strings, functions, tuples, lists
+# Only analyze 1 out of every skip frames for speed
+skip=100
+# Depends on the video
+fps=30
 
 def avg_video(video):
-  if os.path.isfile("frames/mean.jpg"):
-    return Image.open("frames/mean.jpg")
+  if os.path.isfile(mean_file):
+    return Image.open(mean_file)
   vidcap = cv2.VideoCapture(video)
   success,frame=vidcap.read()
   frame=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
@@ -75,7 +25,6 @@ def avg_video(video):
   px1 = [[(0,0,0) for _ in range(frame.size[1])] for _ in range(frame.size[0])]
   mean = Image.new('RGB',frame.size, (0,0,0))
   px = mean.load()
-  skip=100
   while success:
     if frames%skip == 0:
       print(frames)
@@ -93,20 +42,14 @@ def avg_video(video):
   for i in range(mean.size[0]):
       for j in range(mean.size[1]):
         px[i,j] = tuple([v//(frames//skip) for v in px1[i][j]])
-        
-  mean.save("frames/mean.jpg", "JPEG")
-
+  mean.save(mean_file, "JPEG")
   return mean 
 
-def stddev_video(video, mean=None):
-  if os.path.isfile("frames/sd.jpg"):
-    return Image.open("frames/sd.jpg")
-  if mean is None:
-    if os.path.isfile("frames/mean.jpg"):
-      mean = Image.open("frames/mean.jpg")
-    else:
-      mean = avg_video(video)
-    
+
+def stddev_video(video):
+  if os.path.isfile(sd_file):
+    return Image.open(sd_file)
+  mean = avg_video(video)   
   meanpx = mean.load()
   vidcap = cv2.VideoCapture(video)
   success,frame=vidcap.read()
@@ -115,7 +58,6 @@ def stddev_video(video, mean=None):
   px1 = [[(0,0,0) for _ in range(frame.size[1])] for _ in range(frame.size[0])]
   sd = Image.new('RGB',frame.size, (0,0,0))
   px = sd.load()
-  skip=100
   while success:
     if frames%skip == 0:
       print(frames)
@@ -134,8 +76,9 @@ def stddev_video(video, mean=None):
       for j in range(sd.size[1]):
         px[i,j] = tuple([v//(frames//skip) for v in px1[i][j]])
   
-  sd.save("frames/sd.jpg")
+  sd.save(sd_file)
   return sd
+
 
 def z_video(video):
   mean = avg_video(video)
@@ -147,7 +90,6 @@ def z_video(video):
   success,frame=vidcap.read()
   frame=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
   frames = 0
-  skip=100
   while success:
     if frames%skip == 0:
       print(frames)
@@ -167,21 +109,17 @@ def z_video(video):
         z[i][j] = tuple([max(0, int((m-x))) if s!=0 else 0 for (m,s,x) in zip(mean_px[i,j],sd_px[i,j],px2[i,j])])
         z_px[i,j] = tuple([min(255,4*max(0,(int((m-x))))) if s!=0 else 0 for (m,s,x) in zip(mean_px[i,j],sd_px[i,j],px2[i,j])])
     zs.append(z)
-    z_image.save("frames2/z2_%d.jpg"%frames, "JPEG")
+    z_image.save(z_dir+"z_%d.jpg"%frames, "JPEG")
   return zs
-    
 
-z_file = 'pickle_zs'
-points_file = 'frames/points'
-chambers_file = 'frames/chambers'
 
 def cz_video(video):
-  if os.path.isfile(z_file):
-    f = open(z_file, "rb")
+  if os.path.isfile(z_pickle):
+    f = open(z_pickle, "rb")
     zs = load(f)
   else:
     zs = z_video(video)
-    f = open(z_file, 'wb')
+    f = open(z_pickle, 'wb')
     dump(zs, f)
     f.close()
   out = open(points_file, "w")
@@ -194,7 +132,7 @@ def cz_video(video):
         if last is not None:
           if abs(i-last[0])>120 or abs(j-last[1])>120:
             continue
-        a, b, c  = z[i][j]
+        a, b, c = z[i][j]
         wi = a**4 + b**4 + c**4
         w+= wi 
         X+=i*wi
@@ -203,12 +141,15 @@ def cz_video(video):
     out.write("(" +str(X/w)+ ", "+str(Y/w)+")")
   out.close()
 
+
 def which_chamber(video):
   out = open(chambers_file, "w")
   if os.path.isfile(points_file):
     f = open(points_file)
     for line in f:
-      x = float(line.split(", ")[0][1:])
+      x = float(line.split(", ")[0][1:])   
+         
+      #Wall locations hard coded for now    
       if x < 210:
         out.write ("0\n")
       if x >= 210 and x < 405:
@@ -217,14 +158,14 @@ def which_chamber(video):
         out.write("2\n")
   out.close()
 
+
 def time_chamber(video):
   if os.path.isfile(chambers_file):
     f = open(chambers_file)
     counts = [0,0,0]
     for line in f:
       chamber = int(line)
-      counts[chamber]+=3.33
-      
+      counts[chamber]+=skip/fps
     print(counts)
 
       
